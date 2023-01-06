1956e10f7342b5c5905bf10e8bad4a9e2b043924
Fionna Tong <fionna_tong@tech.gov.sg>
2023-01-06 21:51:15 +0800

    feat: choose size of image

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { ChangeEvent, FormEvent, useState } from "react";
+import { CreateImageRequestSizeEnum } from "openai";
 import { generateImage } from "./openai-service";
 import "./App.css";
 
+interface IImageSize {
+	[key: string]: CreateImageRequestSizeEnum;
+}
+
+const ImageSize: IImageSize = {
+	Small: CreateImageRequestSizeEnum._256x256,
+	Medium: CreateImageRequestSizeEnum._512x512,
+	Large: CreateImageRequestSizeEnum._1024x1024,
+};
+
 function App() {
 	const [prompt, setPrompt] = useState<string>("");
+	const [size, setSize] = useState<CreateImageRequestSizeEnum>(CreateImageRequestSizeEnum._256x256);
 	const [loading, setLoading] = useState<boolean>(false);
 	const [error, setError] = useState<boolean>(false);
 	const [imageUrl, setImageUrl] = useState<string | undefined>(undefined);
 
 	const onPromptChange = (e: ChangeEvent<HTMLTextAreaElement>): void => {
 		setPrompt(e.target.value || "");
+	};
+
+	const onSizeChange = (e: ChangeEvent<HTMLSelectElement>): void => {
+		setSize(e.target.value as CreateImageRequestSizeEnum);
 	};
 
 	const onSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
@@ -19,7 +35,7 @@ function App() {
 		setError(false);
 
 		try {
-			const url = await generateImage(prompt);
+			const url = await generateImage(prompt, size);
 			setImageUrl(url);
 		} catch (e) {
 			setError(true);
@@ -41,9 +57,12 @@ function App() {
 					disabled={loading}
 					rows={3}
 				/>
-				<button type="submit" disabled={loading}>
-					Generate!
-				</button>
+				<select name="Image size" id="size" onChange={onSizeChange}>
+					{Object.entries(ImageSize).map(([key, value]) => (
+						<option value={value}>{key}</option>
+					))}
+				</select>
+				<button type="submit" disabled={loading}>Generate!</button>
 			</form>
 			{loading && <p>Loading...</p>}
 			{error && <p>The image could not be generated.</p>}
--- src/openai-service.ts
+++ src/openai-service.ts
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, CreateImageRequestSizeEnum, OpenAIApi } from "openai";
 
 const openaiConfig = new Configuration({
 	apiKey: import.meta.env.VITE_OPENAI_API_KEY,
@@ -6,13 +6,14 @@ const openaiConfig = new Configuration({
 const openai = new OpenAIApi(openaiConfig);
 
 export const generateImage = async (
-	prompt: string
+	prompt: string,
+	size: CreateImageRequestSizeEnum
 ): Promise<string | undefined> => {
 	try {
 		const response = await openai.createImage({
 			prompt,
 			n: 1,
-			size: "512x512",
+			size,
 		});
 		return response.data.data[0].url;
 	} catch (e: any) {
