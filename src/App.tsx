513b46abd35f252bacfa167a0154f6105b982e11
Fionna Tong <fionna_tong@tech.gov.sg>
2023-01-06 20:26:58 +0800

    feat: add input for prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ChangeEvent, FormEvent, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [prompt, setPrompt] = useState("");
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  const onPromptChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setPrompt(e.target.value || '')
+  }
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+  }
+
+	return (
+		<div className="App">
+			<h1>Image Generator</h1>
+      <form onSubmit={onSubmit}>
+        <input id="prompt" type="text" value={prompt} placeholder="Enter prompt here" onChange={onPromptChange} />
+        <button type="submit">Generate!</button>
+      </form>
+		</div>
+	);
 }
 
-export default App
+export default App;
